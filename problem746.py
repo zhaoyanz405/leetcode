5d60e7837912684c301faced54ebde7248e5754b
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-04-15 18:51:01 +0800

    feat: resolve problem 746

---
+++ problem746.py
@@ -0,0 +1,28 @@
+"""
+给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+
+你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+
+请你计算并返回达到楼梯顶部的最低花费。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost) -> int:
+        if len(cost) <= 1:
+            return 0
+
+        dp = [0] * (len(cost) + 1)
+        for i in range(2, len(cost) + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minCostClimbingStairs([10, 15, 20]))
