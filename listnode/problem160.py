fdb705a83b2c66bf6ff0ca6ac91b7baebf2c30b9
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-05-08 17:02:08 +0800

    feat: resolve problem 160

---
+++ listnode/problem160.py
@@ -0,0 +1,22 @@
+"""
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回null。
+"""
+from listnode import ListNode
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        p1 = headA
+        p2 = headB
+        while p1 != p2:
+            if p1:
+                p1 = p1.next
+            else:
+                p1 = headB
+
+            if p2:
+                p2 = p2.next
+            else:
+                p2 = headA
+
+        return p1
