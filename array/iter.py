47e17e9db1b64b58e045f66252a888648523eeb7
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-05-10 11:19:48 +0800

    resolve: rotate a matrix ccw

--- array/iter.py
+++ array/iter.py
@@ -3,7 +3,7 @@ def print_matrix(m):
         print(row)
 
 
-def rotate_matrix(matrix):
+def rotate_matrix_cw(matrix):
     """
     将二维矩阵顺时针转动90度
     :param matrix:
@@ -20,11 +20,27 @@ def rotate_matrix(matrix):
         matrix[idx].reverse()
 
 
+def rotate_matrix_ccw(matrix):
+    """
+    逆时针旋转矩阵90度
+    :param matrix:
+    :return:
+    """
+    n = len(matrix)
+    # 1. 对折
+    for i in range(n):
+        for j in range(n - i):
+            matrix[i][j], matrix[n - 1 - j][n - 1 - i] = matrix[n - 1 - j][n - 1 - i], matrix[i][j]
+
+    for idx in range(n):
+        matrix[idx].reverse()
+
+
 if __name__ == '__main__':
     test_matrix = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9],
     ]
-    rotate_matrix(test_matrix)
+    rotate_matrix_ccw(test_matrix)
     print_matrix(test_matrix)
